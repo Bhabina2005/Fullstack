a1c199f1acd198ea8d4c7538c477e3c5fb427f3b
Bhabina2005 <bhabinababu@gmail.com>
2026-07-17 15:22:46 +0530

    17-07-2026

---
+++ accounts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+    search_fields = ('username', 'email')
+    ordering = ('email',)
+    # extend the default UserAdmin fieldsets with extra fields
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('bio', 'profile_pic')}),
+    )
+
+
+# register at module level
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ books/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Book, Review
+
+
+# Register your models here.
+class ReviewInline(admin.TabularInline):
+    model = Review
+    fk_name = 'book'
+    extra = 1
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'added_by', 'created_at']
+    search_fields = ['title', 'author']
+    inlines = [ReviewInline]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'book', 'rating']
+    list_filter = ['rating']
